8d5295e07b2ab4625d6f2ad7a325d9269c241112
Daniel Naaman <danielnaaman@icloud.com>
2020-09-17 16:35:17 +0300

    Finish part one

--- Concentration/Form1.Designer.cs
+++ Concentration/Form1.Designer.cs
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.card_1 = new System.Windows.Forms.PictureBox();
+            this.card_2 = new System.Windows.Forms.PictureBox();
+            this.card_3 = new System.Windows.Forms.PictureBox();
+            this.card_4 = new System.Windows.Forms.PictureBox();
+            this.card_5 = new System.Windows.Forms.PictureBox();
+            this.card_6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.card_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // card_1
+            // 
+            this.card_1.Image = global::Concentration.Properties.Resources.shawarma;
+            this.card_1.Location = new System.Drawing.Point(12, 12);
+            this.card_1.Name = "card_1";
+            this.card_1.Size = new System.Drawing.Size(195, 194);
+            this.card_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card_1.TabIndex = 0;
+            this.card_1.TabStop = false;
+            this.card_1.Click += new System.EventHandler(this.card_1_Click);
+            // 
+            // card_2
+            // 
+            this.card_2.Image = global::Concentration.Properties.Resources.shawarma;
+            this.card_2.Location = new System.Drawing.Point(213, 12);
+            this.card_2.Name = "card_2";
+            this.card_2.Size = new System.Drawing.Size(195, 194);
+            this.card_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card_2.TabIndex = 1;
+            this.card_2.TabStop = false;
+            this.card_2.Click += new System.EventHandler(this.card_2_Click);
+            // 
+            // card_3
+            // 
+            this.card_3.Image = global::Concentration.Properties.Resources.shawarma;
+            this.card_3.Location = new System.Drawing.Point(414, 12);
+            this.card_3.Name = "card_3";
+            this.card_3.Size = new System.Drawing.Size(195, 194);
+            this.card_3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card_3.TabIndex = 2;
+            this.card_3.TabStop = false;
+            this.card_3.Click += new System.EventHandler(this.card_3_Click);
+            // 
+            // card_4
+            // 
+            this.card_4.Image = global::Concentration.Properties.Resources.shawarma;
+            this.card_4.Location = new System.Drawing.Point(12, 212);
+            this.card_4.Name = "card_4";
+            this.card_4.Size = new System.Drawing.Size(195, 194);
+            this.card_4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card_4.TabIndex = 3;
+            this.card_4.TabStop = false;
+            this.card_4.Click += new System.EventHandler(this.card_4_Click);
+            // 
+            // card_5
+            // 
+            this.card_5.Image = global::Concentration.Properties.Resources.shawarma;
+            this.card_5.Location = new System.Drawing.Point(213, 212);
+            this.card_5.Name = "card_5";
+            this.card_5.Size = new System.Drawing.Size(195, 194);
+            this.card_5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card_5.TabIndex = 4;
+            this.card_5.TabStop = false;
+            this.card_5.Click += new System.EventHandler(this.card_5_Click);
+            // 
+            // card_6
+            // 
+            this.card_6.Image = global::Concentration.Properties.Resources.shawarma;
+            this.card_6.Location = new System.Drawing.Point(414, 212);
+            this.card_6.Name = "card_6";
+            this.card_6.Size = new System.Drawing.Size(195, 194);
+            this.card_6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.card_6.TabIndex = 5;
+            this.card_6.TabStop = false;
+            this.card_6.Click += new System.EventHandler(this.card_6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(619, 418);
+            this.Controls.Add(this.card_6);
+            this.Controls.Add(this.card_5);
+            this.Controls.Add(this.card_4);
+            this.Controls.Add(this.card_3);
+            this.Controls.Add(this.card_2);
+            this.Controls.Add(this.card_1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.card_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.card_6)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox card_1;
+        private System.Windows.Forms.PictureBox card_2;
+        private System.Windows.Forms.PictureBox card_3;
+        private System.Windows.Forms.PictureBox card_4;
+        private System.Windows.Forms.PictureBox card_5;
+        private System.Windows.Forms.PictureBox card_6;
     }
 }
 
--- Concentration/Form1.cs
+++ Concentration/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Concentration.Properties;
 
 namespace Concentration
 {
@@ -15,6 +16,109 @@ namespace Concentration
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private bool isBack0 = true;
+        private bool isBack1 = true;
+        private bool isBack2 = true;
+        private bool isBack3 = true;
+        private bool isBack4 = true;
+        private bool isBack5 = true;
+
+        private void card_1_Click(object sender, EventArgs e)
+        {
+            {
+                if (!isBack0)
+                {
+                    card_1.Image = Resources.shawarma;
+                    isBack0 = true;
+                }
+                else
+                {
+                    card_1.Image = Resources.green_banana;
+                    isBack0 = false;
+                }
+            }
+        }
+
+        private void card_2_Click(object sender, EventArgs e)
+        {
+            {
+                if (!isBack1)
+                {
+                    card_2.Image = Resources.shawarma;
+                    isBack1 = true;
+                }
+                else
+                {
+                    card_2.Image = Resources.green_banana;
+                    isBack1 = false;
+                }
+            }
+        }
+
+        private void card_3_Click(object sender, EventArgs e)
+        {
+            {
+                if (!isBack2)
+                {
+                    card_3.Image = Resources.shawarma;
+                    isBack2 = true;
+                }
+                else
+                {
+                    card_3.Image = Resources.green_banana;
+                    isBack2 = false;
+                }
+            }
+        }
+
+        private void card_4_Click(object sender, EventArgs e)
+        {
+            {
+                if (!isBack3)
+                {
+                    card_4.Image = Resources.shawarma;
+                    isBack3 = true;
+                }
+                else
+                {
+                    card_4.Image = Resources.green_banana;
+                    isBack3 = false;
+                }
+            }
+        }
+
+        private void card_5_Click(object sender, EventArgs e)
+        {
+            {
+                if (!isBack4)
+                {
+                    card_5.Image = Resources.shawarma;
+                    isBack4 = true;
+                }
+                else
+                {
+                    card_5.Image = Resources.green_banana;
+                    isBack4 = false;
+                }
+            }
+        }
+
+        private void card_6_Click(object sender, EventArgs e)
+        {
+            {
+                if (!isBack5)
+                {
+                    card_6.Image = Resources.shawarma;
+                    isBack5 = true;
+                }
+                else
+                {
+                    card_6.Image = Resources.green_banana;
+                    isBack5 = false;
+                }
+            }
         }
     }
 }
